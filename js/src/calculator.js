5ff1ac047662a9ea99fc5ccf97f1714b4eed614f
James Pederson <james@jpederson.com>
2014-05-20 13:19:42 -0500

    Removing commas from the totals before totalling to prevent NaN result - in the rare case where one of the savings amounts is over $1,000.

--- js/src/calculator.js
+++ js/src/calculator.js
@@ -3,9 +3,9 @@
 $(function(){
 	
 	var recalculate_total = function(){
-			var total = parseFloat( typeof( $(".result.credit").html() )!=="undefined" ? $(".result.credit").html().replace("$","") : 0 )+
-				parseFloat( typeof( $(".result.loan-auto").html() )!=="undefined" ? $(".result.loan-auto").html().replace("$","") : 0 )+
-				parseFloat( typeof( $(".result.loan-personal").html() )!=="undefined" ? $(".result.loan-personal").html().replace("$","") : 0 );
+			var total = parseFloat( typeof( $(".result.credit").html() )!=="undefined" ? $(".result.credit").html().replace("$","").replace(",","") : 0 )+
+				parseFloat( typeof( $(".result.loan-auto").html() )!=="undefined" ? $(".result.loan-auto").html().replace("$","").replace(",","") : 0 )+
+				parseFloat( typeof( $(".result.loan-personal").html() )!=="undefined" ? $(".result.loan-personal").html().replace("$","").replace(",","") : 0 );
 			if ( total>0 ) {
 				$(".result.total").html("Save up to $"+total.toFixed(2)+"!");
 			} else {
