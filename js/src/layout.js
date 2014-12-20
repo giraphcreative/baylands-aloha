075395531b39391e1139f3dad6a4f0d07b3648ca
James Pederson <james@jpederson.com>
2014-12-20 00:48:39 -0600

    Adding and fixing fittext, adjusting more styles for the results area, all three calculator divs, and adding images in for each of those calculators.

--- js/src/layout.js
+++ js/src/layout.js
@@ -2,14 +2,16 @@
 
 $(function(){
 
-	$('.header h1').fitText( 1.3 );
-	$('.header h2').fitText( 3.1 );
+	$('.header h1').fitText( 1.3, { minFontSize: 40 } );
+	$('.header h2').fitText( 3.1, { minFontSize: 20 } );
 
 
-	$('.more-info h3').fitText( 2 );
-	$('.more-info p').fitText( 2.5 );
+	$('.more-info h3').fitText( 2, { minFontSize: 30 } );
+	$('.more-info p').fitText( 2.5, { minFontSize: 20 } );
 
-	$('.footer-bar').fitText( 3.1 );
+	$('.rate-display h2').fitText( 0.2, { matchMedia: 'only screen and (min-width: 768px)' } );
+
+	$('.footer-bar').fitText( 3.3, { matchMedia: 'only screen and (min-width: 768px)', maxFontSize: 40 });
 
 });
 
