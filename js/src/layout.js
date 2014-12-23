22f265a7b65ab2a97abc69f7c88dd02dfd562e4a
James Pederson <james@jpederson.com>
2014-12-23 16:20:31 -0600

    Add apply/reset buttons, fix some styling for the right column, adjust rate font size so it doesn't cut off "% APR" text, and adjust footer styles and disclaimer.

--- js/src/layout.js
+++ js/src/layout.js
@@ -3,15 +3,15 @@
 $(function(){
 
 	$('.header h1').fitText( 1.3, { minFontSize: 40 } );
-	$('.header h2').fitText( 3.1, { minFontSize: 20 } );
+	$('.header h2').fitText( 4.2, { minFontSize: 20 } );
 
 
 	$('.more-info h3').fitText( 2, { minFontSize: 30 } );
 	$('.more-info p').fitText( 2.5, { minFontSize: 20 } );
 
-	$('.rate-display h2').fitText( 0.2, { matchMedia: 'only screen and (min-width: 768px)' } );
+	$('.rate-display h2').fitText( 0.3, { matchMedia: 'only screen and (min-width: 768px)' } );
 
-	$('.footer-bar').fitText( 3.3, { matchMedia: 'only screen and (min-width: 768px)', maxFontSize: 40 });
+	$('.footer-bar').fitText( 3.6, { matchMedia: 'only screen and (min-width: 768px)', maxFontSize: 40 });
 
 });
 
